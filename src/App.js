91fe8487d0e866cc2429b08f9ffbfda13828daf7
rock's ezweb <alikerock@gmail.com>
2024-05-27 12:01:11 +0900

    mynav,myartcle 추가

--- src/App.js
+++ src/App.js
@@ -19,11 +19,38 @@ class Myheader extends Component {
     )
   }
 }
+class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+            <li><a href="">HTML</a></li>
+            <li><a href="">CSS</a></li>
+            <li><a href="">JavaScript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+class Myarticle extends Component {
+  render() {
+    return (
+      <section>
+        <article>
+              <h2>HTML</h2>
+              <p>Hypertext markup language</p>
+          </article>
+      </section>
+    )
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Myheader></Myheader>
+        <Myheader/>
+        <Mynav/>
+        <Myarticle/>
       </div>
     )
   }
